6c1c765e7ef921768d4fc4594301966e44b1e625
sandeepdhama022 <sandeepdhama0894@gmail.com>
2023-11-09 17:50:12 +0530

    practice project 4

--- 04ProjectTailwind/src/App.jsx
+++ 04ProjectTailwind/src/App.jsx
@@ -2,34 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold bg-green-400 p-4 rounded-xl">
+      <h1 className="text-3xl font-bold bg-green-400 p-4 rounded-xl mb-5">
         Hello world!
       </h1>
-      <div class="relative h-[400px] w-[300px] rounded-md">
-        <img
-          src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-          alt="AirMax Pro"
-          class="z-0 h-full w-full rounded-md object-cover"
-        />
-        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-        <div class="absolute bottom-4 left-4 text-left">
-          <h1 class="text-lg font-semibold text-white">Delba</h1>
-          <p class="mt-2 text-sm text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-            debitis?
-          </p>
-          <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-            View Profile →
-          </button>
-        </div>
-      </div>
-
+      <Card name='Sandeep Dhama' btnText='Open Web' />
+      <Card name='Lalita Dhama' btnText='Click Me' />
+      
     </>
   )
 }
