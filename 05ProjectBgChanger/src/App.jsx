b2d06a83da3e1ba4c0baaa64e706b030a69f4c85
sandeepdhama022 <sandeepdhama0894@gmail.com>
2023-11-10 14:44:39 +0530

    working project 5

--- 05ProjectBgChanger/src/App.jsx
+++ 05ProjectBgChanger/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div width-full style={backgroundColor-white}>
+      <div className='container'>
+        <h1>Sandeep Dhama</h1>
 
       </div>
     </>
