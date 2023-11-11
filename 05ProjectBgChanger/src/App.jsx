7cc81afd8fbf7108e365fc3db2d01fb1c4c2a190
sandeepdhama022 <sandeepdhama0894@gmail.com>
2023-11-11 17:45:18 +0530

    project 5

--- 05ProjectBgChanger/src/App.jsx
+++ 05ProjectBgChanger/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("purple")
 
   return (
-    <>
-      <div className='flex items-center text-center font-bold border-4 mt-4 w-80'>
-        <h1>Sandeep Dhama</h1>
+    <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-10 inset-x-0">
+        <div className="flex flex-wrap justify-center gap-4 bg-white rounded-xl px-3 py-2">
+          <button
+            className="outline-none px-4 py-1 rounded-xl"
+            style={{backgroundColor:"red", color:"white"}}>
+              Red
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-xl"
+            style={{backgroundColor:"green", color:"white"}}>
+              Green
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
