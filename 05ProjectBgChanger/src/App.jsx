59f26af55206748cff4b84b11f1bedc4487999b5
sandeepdhama022 <sandeepdhama0894@gmail.com>
2023-11-12 23:41:16 +0530

    project 5 completed

--- 05ProjectBgChanger/src/App.jsx
+++ 05ProjectBgChanger/src/App.jsx
@@ -5,77 +5,77 @@ function App() {
 
   return (
     <div className="w-full h-screen duration-250" style={{backgroundColor: color}}>
-      <div className="fixed top-5 right-4">
-        <div className="w-24 h-auto bg-white m-10 rounded-xl gap-4">
+      <div className="fixed top-10 right-5">
+        <div className="rounded-xl gap-4" style={{backgroundColor:"white"}}>
           <button
             onClick={()=>{setColor('red')}}
-            className="outline-none px-4 py-1 m-1 rounded-xl"
+            className="outline-none px-4 py-1 mx-6 my-1 mt-4 rounded-xl"
             style={{backgroundColor:"red", color:'white'}}>
               Red
-          </button>
+          </button><br />
           <button
             onClick={()=>{setColor('green')}}
-            className="outline-none px-4 py-1 rounded-xl"
+            className="outline-none px-4 py-1 mx-6 my-1 rounded-xl"
             style={{backgroundColor:"green", color:'white'}}>
               Green
-          </button>
+          </button><br />
           <button
             onClick={()=>{setColor('blue')}}
-            className="outline-none px-4 py-1 rounded-xl"
+            className="outline-none px-4 py-1 mx-6 my-1 rounded-xl"
             style={{backgroundColor:"blue", color:'white'}}>
               Blue
-          </button>
+          </button><br />
           <button
             onClick={()=>{setColor('olive')}}
-            className="outline-none px-4 py-1 rounded-xl"
+            className="outline-none px-4 py-1 mx-6 my-1 rounded-xl"
             style={{backgroundColor:"olive", color:'white'}}>
               Olive
-          </button>
+          </button><br />
           <button
             onClick={()=>{setColor('yellow')}}
-            className="outline-none px-4 py-1 rounded-xl"
+            className="outline-none px-4 py-1 mx-6 my-1 rounded-xl"
             style={{backgroundColor:"yellow", color:'black'}}>
               Yellow
-          </button>
+          </button><br />
           <button
             onClick={()=>{setColor('navy')}}
-            className="outline-none px-4 py-1 rounded-xl"
+            className="outline-none px-4 py-1 mx-6 my-1 rounded-xl"
             style={{backgroundColor:"navy", color:'white'}}>
               Navy
-          </button>
+          </button><br />
           <button
             onClick={()=>{setColor('teal')}}
-            className="outline-none px-4 py-1 rounded-xl"
+            className="outline-none px-4 py-1 mx-6 my-1 rounded-xl"
             style={{backgroundColor:"teal", color:'white'}}>
               Teal
-          </button>
+          </button><br />
           <button
             onClick={()=>{setColor('aqua')}}
-            className="outline-none px-4 py-1 rounded-xl"
+            className="outline-none px-4 py-1 mx-6 my-1 rounded-xl"
             style={{backgroundColor:"aqua", color:'black'}}>
               Aqua
-          </button>
+          </button><br />
           <button
             onClick={()=>{setColor('gold')}}
-            className="outline-none px-4 py-1 rounded-xl"
+            className="outline-none px-4 py-1 mx-6 my-1 rounded-xl"
             style={{backgroundColor:"gold", color:'black'}}>
               Gold
-          </button>
+          </button><br />
           <button
             onClick={()=>{setColor('grey')}}
-            className="outline-none px-4 py-1 rounded-xl"
+            className="outline-none px-4 py-1 mx-6 my-1 rounded-xl"
             style={{backgroundColor:"grey", color:'white'}}>
               Grey
-          </button>
+          </button><br />
           <button
             onClick={()=>{setColor('pink')}}
-            className="outline-none px-4 py-1 rounded-xl"
+            className="outline-none px-4 py-1 mx-6 my-1 rounded-xl"
             style={{backgroundColor:"pink", color:'black'}}>
               Pink
-          </button>
+          </button><br />
           <button
             onClick={()=>{setColor('purple')}}
-            className="outline-none px-4 py-1 rounded-xl"
+            className="outline-none px-4 py-1 mx-6 my-1 mb-4 rounded-xl"
             style={{backgroundColor:"purple", color:'white'}}>
               Purple
           </button>
