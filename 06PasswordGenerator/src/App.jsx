ae7187285a025422db5d7b82c77aafad3a6bbeb5
sandeepdhama022 <sandeepdhama0894@gmail.com>
2023-11-22 23:26:40 +0530

    working project 5

--- 06PasswordGenerator/src/App.jsx
+++ 06PasswordGenerator/src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
+import { useCallback, useState, useEffect } from 'react'
 
 
 function App() {
-  const [length, setLength] = useState(8)
+  const [length, setLength] = useState(4)
+  const [password, setPassword] = useState("")
   const [numAllowed, setNumAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-  let pass = ""
-  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numAllowed) {
+      str += '0123456789'
+    }
+    if (charAllowed) {
+      str += '!@#$_+-*/~'
+    }
 
-  if(numAllowed){
-    str += '0123456789'
-  }
-  if(charAllowed){
-    str += '!@#$_+-*/~'
-  }
+    for (let i = 1; i <= length; i++) {
+      let charNum = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt[charNum]
+    }
+    setPassword(pass)
+  }, [length, numAllowed, charAllowed])
 
-  for (let i = 1; i <= length; i++) {
-    let charNum = Math.floor(Math.randomNum() * str.length) + 1
-    pass += str.charAt[i]
-  }
+  useEffect(()=>{
+    passwordGenerator()
+  },[length, numAllowed, charAllowed, passwordGenerator])
 
   return (
     <>
       <div className='w-full max-w-2xl mx-auto my-10 p-5 rounded-xl bg-gray-800 text-white text-center'>
         <h1 className='text-4xl text-white'>Password Generator</h1>
-        <input type="text" className='w-96 my-5 mx-1 p-2 rounded-md text-red-600' />
+        <input type="text" className='w-96 my-5 mx-1 p-2 rounded-md text-red-600' readOnly placeholder='Password' value={password} />
         <button className='bg-red-600 text-white p-2 rounded-md'>Copy</button><br />
-        <input type="range" name="length" id="length" min={'4'} max={'50'} />
+        <input type="range" name="length" id="length"
+        min={'2'} max={'50'}
+        onChange={(e)=>{setLength(e.target.value)}} />
         <label htmlFor="length" className='mx-2'>Length ({length})</label>
         <input type="checkbox" name="num" id="num" className='mx-2' />
         <label htmlFor="num">Numbers</label>
