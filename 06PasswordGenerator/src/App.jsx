38c79cd49c1cd1fa4d5a4104fb662f4e7e84362a
sandeepdhama022 <sandeepdhama0894@gmail.com>
2023-11-24 23:12:53 +0530

    project 6 completed

--- 06PasswordGenerator/src/App.jsx
+++ 06PasswordGenerator/src/App.jsx
@@ -1,8 +1,8 @@
-import { useCallback, useState, useEffect } from 'react'
+import { useCallback, useState, useEffect, useRef } from 'react'
 
 
 function App() {
-  const [length, setLength] = useState(4)
+  const [length, setLength] = useState(8)
   const [password, setPassword] = useState("")
   const [numAllowed, setNumAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
@@ -30,20 +30,28 @@ function App() {
     passwordGenerator()
   },[length, numAllowed, charAllowed, passwordGenerator])
 
+  const passRef = useRef(null)
+
+  function copyToClipboard(){
+    window.navigator.clipboard.writeText(password)
+    passRef.current?.select()
+  }
+
   return (
     <>
       <div className='w-full max-w-2xl mx-auto my-10 p-5 rounded-xl bg-gray-800 text-white text-center'>
         <h1 className='text-4xl text-white'>Password Generator</h1>
-        <input type="text" className='w-96 my-5 mx-1 p-2 rounded-md text-red-600' readOnly placeholder='Password' value={password} />
-        <button className='bg-red-600 text-white p-2 rounded-md'>Copy</button><br />
+        <input type="text" className='w-96 my-5 mx-1 p-2 rounded-md text-red-600' readOnly placeholder='Password' value={password} ref={passRef}/>
+        <button className='bg-red-600 text-white p-2 rounded-md'
+        onClick={copyToClipboard}>Copy</button><br />
         <input type="range" name="length" id="length"
-        min={'2'} max={'50'}
+        min={'4'} max={'50'} value={length}
         onChange={(e)=>{setLength(e.target.value)}} />
         <label htmlFor="length" className='mx-2'>Length ({length})</label>
-        <input type="checkbox" name="num" id="num" className='mx-2'
+        <input type="checkbox" name='num' id='num' className='mx-2'
         onChange={()=>{setNumAllowed((prev) => !prev)}}/>
         <label htmlFor="num">Numbers</label>
-        <input type="checkbox" name="char" id="char" className='mx-2'
+        <input type="checkbox" name='char' id='char' className='mx-2'
         onChange={()=>{setCharAllowed((prev) => !prev)}}/>
         <label htmlFor="char">Characters</label>
       </div>
