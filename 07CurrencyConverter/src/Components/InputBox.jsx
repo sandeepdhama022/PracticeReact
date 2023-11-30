384544cf8f32f9f8b3df8b17a054e734bcefb3ff
sandeepdhama022 <sandeepdhama0894@gmail.com>
2023-11-30 22:29:57 +0530

    working project 7

--- 07CurrencyConverter/src/Components/InputBox.jsx
+++ 07CurrencyConverter/src/Components/InputBox.jsx
@@ -16,10 +16,10 @@ function InputBox({
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
-                <input
+                <input id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
